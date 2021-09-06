b79405d6273f7e0fbaed2388810411b1a3e75150
fisk810 <marc571m@edu.easj.dk>
2021-09-06 11:03:26 +0200

    Man kan nu vælge tal op til 10, hvis højere giver den fejl besked og kører igen

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,20 +6,27 @@ public class Main {
         Scanner tif = new Scanner(System.in);
         int rnumber = 0;
         int sum = 0;
-        System.out.println("Skriv antal gange du vil rulle en terning\n");
-        int numberOfRolls = tif.nextInt();
-        for (int i = 0; i < numberOfRolls ; i++) {
-            System.out.println("---------------------------\n");
-            System.out.println((i+1)+". gang du kaster terningen slår du:\n" );
-            rnumber =(int)(Math.ceil(Math.random() * 6));
-            sum += rnumber;
-            System.out.print(rnumber + "\n");
-            System.out.println("Summen af dine terningekast er:" + sum);
-            if (rnumber == 6){
-                System.out.print("Fedt, du slog en 6'er\n");
+        while(true) {
+            System.out.println("Skriv antal gange du vil rulle en terning\n");
+            int numberOfRolls = tif.nextInt();
+
+            if (numberOfRolls <= 10) {
+                for (int i = 0; i < numberOfRolls; i++) {
+                    System.out.println("---------------------------\n");
+                    System.out.println((i + 1) + ". gang du kaster terningen slår du:\n");
+                    rnumber = (int) (Math.ceil(Math.random() * 6));
+                    sum += rnumber;
+                    System.out.print(rnumber + "\n");
+                    System.out.println("Summen af dine terningekast er:\n" + sum);
+
+                    if (rnumber == 6) {
+                        System.out.print("Fedt, du slog en 6'er\n");
+                    }
+                }
+            } else {
+                System.out.println("dit tal er ikke mindre end 10");
             }
-
-
+            sum = 0;
         }
 
 
