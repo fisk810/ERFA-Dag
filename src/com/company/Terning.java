effdabd3cbb0dc8d27f14c565957f36542820ea4
fisk810 <marc571m@edu.easj.dk>
2021-09-06 12:18:26 +0200

    Vi har givet terningen ikke standard sider

--- src/com/company/Terning.java
+++ src/com/company/Terning.java
@@ -1,8 +1,15 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Queue;
+
 public class Terning {
-    private int antalSider = 6;
+    private final int ANTALSIDER = 5;
+    List<Integer> sideVaerdier= Arrays.asList(7,9,13,18,21);
     private int nuvaerendeVaerdi = 0;
+    private int valgtSide = 0;
 
 
     public int getNuvaerendeVaerdi() {
@@ -12,7 +19,8 @@ public class Terning {
     public void terningKast(){
             System.out.println("---------------------------\n");
             System.out.println("du slog:\n");
-            nuvaerendeVaerdi = (int) (Math.ceil(Math.random() * antalSider));
+            valgtSide = ((int) (Math.ceil(Math.random() * ANTALSIDER)) -1);
+            nuvaerendeVaerdi = sideVaerdier.get(valgtSide);
             System.out.print(nuvaerendeVaerdi + "\n");
 
             if (nuvaerendeVaerdi == 6) {
