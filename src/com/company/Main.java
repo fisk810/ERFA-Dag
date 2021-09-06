4169fe0979186f699e89d33d7f0f5ea7f5bee9fb
fisk810 <marc571m@edu.easj.dk>
2021-09-06 10:39:17 +0200

    Vi kan  nu skrive antal gange vi kaster en terning

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,21 +5,17 @@ public class Main {
     public static void main(String[] args) {
         Scanner tif = new Scanner(System.in);
         int rnumber = 0;
-        System.out.println("Skriv x for at rulle en terning\n");
-        while(true) {
-            String next = tif.nextLine();
-            if (next.equals("x")) {
-
-                rnumber =(int)(Math.ceil(Math.random() * 6));
-                System.out.print(rnumber + "\n");
-            }
+        System.out.println("Skriv antal gange du vil rulle en terning\n");
+        int numberOfRolls = tif.nextInt();
+        for (int i = 0; i < numberOfRolls ; i++) {
+            rnumber =(int)(Math.ceil(Math.random() * 6));
+            System.out.print(rnumber + "\n");
             if (rnumber == 6){
                 System.out.print("Fedt, du slog en 6'er");
             }
             rnumber = 0;
-        }
 
-        //for(int e = 0, e < 11; 1++);
+        }
 
 
     }
