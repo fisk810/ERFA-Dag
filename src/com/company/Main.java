45a5464e79c6e1d24d6ebf5727e39703345bfc1b
fisk810 <marc571m@edu.easj.dk>
2021-09-06 10:16:45 +0200

    Et basalt terningekast konsolprogram

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,26 @@
 package com.company;
-
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner tif = new Scanner(System.in);
+        int rnumber = 0;
+        System.out.println("Skriv x for at rulle en terning\n");
+        while(true) {
+            String next = tif.nextLine();
+            if (next.equals("x")) {
+
+                rnumber =(int)(Math.ceil(Math.random() * 6));
+                System.out.print(rnumber + "\n");
+            }
+            if (rnumber == 6){
+                System.out.print("Fedt, du slog en 6'er");
+            }
+            rnumber = 0;
+        }
+
+        //for(int e = 0, e < 11; 1++);
+
+
     }
 }
